64b1fc5fa043d4eeea35f1c01ef22e180e399f1c
Alan DeGuzman <alandeguz@users.noreply.github.com>
2026-07-20 01:34:39 -0500

    FaviconSet - add missing initializers

--- Sources/Publish/API/FaviconSet.swift
+++ Sources/Publish/API/FaviconSet.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// An extended representation of a website's "favicon" (a small icon typically
+/// An extended representation of a website's "favicons" (a small icon typically
 /// displayed along the website's title in various browser UIs).
 public struct FaviconSet {
     
@@ -15,9 +15,22 @@ public struct FaviconSet {
     public var svg: Path?
     public var appleTouchIcon: AppleTouchIcon?
     
+    public init(ico: Path? = nil,
+                svg: Path? = nil,
+                appleTouchIcon: AppleTouchIcon? = nil) {
+        self.ico = ico
+        self.svg = svg
+        self.appleTouchIcon = appleTouchIcon
+    }
+    
     public struct AppleTouchIcon {
         let path: Path
         let size: Int
+        
+        public init(path: Path, size: Int) {
+            self.path = path
+            self.size = size
+        }
     }
     
 }
